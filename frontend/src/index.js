ab0fa533c2297fd4c78c36fbbc965e7c84bf73ce
ayuhsupadhyay <ayushupadhyay345@gmail.com>
2024-03-09 23:45:52 +0530

    vercel

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import axios  from 'axios';
-axios.defaults.baseURL="https://vercel.com/ayuhsupadhyays-projects/scaler-intership-frontend-backend";
+axios.defaults.baseURL="https://scaler-intership-frontend-backend.vercel.app";
 axios.defaults.withCredentials=true;
 
 
